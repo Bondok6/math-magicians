6d8bf83a79dde72f45d39c9da36d2744113b5a39
Bondok6 <kirolousbondok6@gmail.com>
2022-02-10 03:44:00 +0200

    Create logic folder that contains operate.js and calculate.js files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import Calculator from './Calculator';
+import Calculator from './components/Calculator';
 
 class App extends Component {
   constructor(props) {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './components/App';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
