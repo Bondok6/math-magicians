86e9ba40038c78b4c66267870bd584ab7a24930c
Carlos Herver <carlos.h.s.1994@gmail.com>
2022-02-16 13:40:53 -0600

    Add test for calculate.js

---
+++ src/__tests__/logic/calculate.test.js
@@ -0,0 +1,35 @@
+import calculate from '../../logic/calculate';
+
+test('Evaluate operations', () => {
+  let obj = {
+    total: '100',
+    next: '5',
+    operation: '+',
+  };
+  const button = 'AC';
+  expect(calculate(obj, button)).toEqual({
+    total: null,
+    next: null,
+    operation: null,
+  })
+  expect(calculate(obj, '=')).toEqual({
+    total: '105',
+    next: null,
+    operation: null,
+  })
+  expect(calculate(obj, '+/-')).toEqual({
+    total: '100',
+    next: '-5',
+    operation: '+',
+  })
+  obj = {
+    total: '100',
+    next: '5',
+    operation: '÷',
+  }
+  expect(calculate(obj, '=')).toEqual({
+    total: '20',
+    next: null,
+    operation: null,
+  })
+})
